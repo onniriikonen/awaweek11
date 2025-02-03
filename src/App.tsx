67ab1e14daac20e9a02bdc3dd07cad9c00667e3d
enkikkeli <onni.riikonen@student.lut.fi>
2025-02-03 12:51:15 +0200

    1

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Header from "./components/Header"
+import Home from "./components/Home"
+import Saved from "./components/Saved"
+
+
+function App() {
+  return (
+    <div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/saved" element={<Saved />} />
+      </Routes>
+    </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
